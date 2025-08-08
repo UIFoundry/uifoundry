745a2ea1e948b6a308f3609a300bcced8e718d87
Isaiah Anyimi <zaye@Mac-174.lan>
2025-08-07 23:32:51 -0700

    chore: update to latest nextjs canary version

--- next.config.mjs
+++ next.config.mjs
@@ -8,8 +8,8 @@ import { withPayload } from "@payloadcms/next/withPayload";
 /** @type {import("next").NextConfig} */
 const nextConfig = {
 	serverExternalPackages: ["mongodb"],
+	nodeMiddleware: true,
 	experimental: {
-		nodeMiddleware: true,
 		reactCompiler: true,
 	},
 };
