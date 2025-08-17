30628ca88eca4e2222c08be151895a2c65b380f1
Isaiah Anyimi <41877924+ianyimi@users.noreply.github.com>
2025-08-17 01:05:44 -0700

    build err fix

--- src/payload/blocks/Footer/Footer_1/index.tsx
+++ src/payload/blocks/Footer/Footer_1/index.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Link from "next/link";
 import { type ComponentPropsWithRef } from "react";
 import type { Footer_1_Block, Media } from "~/payload-types";
@@ -9,48 +7,48 @@ import Image from "next/image";
 export * from "./config";
 
 export default function Footer_1({
-  brandLogo,
-  links,
-  socialLinks,
-  copyright,
-  ...divProps
+	brandLogo,
+	links,
+	socialLinks,
+	copyright,
+	...divProps
 }: Footer_1_Block & ComponentPropsWithRef<"div">) {
-  return (
-    <footer {...divProps}>
-      <div className="mx-auto max-w-5xl px-6 py-8 md:py-16">
-        <Link href="/" aria-label="go home" className="mx-auto block size-fit">
-          {}
-          {brandLogo && (
-            <Image
-              src={(brandLogo as Media).url!}
-              alt={(brandLogo as Media).alt}
-              width={200}
-              height={150}
-            />
-          )}
-        </Link>
+	return (
+		<footer {...divProps}>
+			<div className="mx-auto max-w-5xl px-6 py-8 md:py-16">
+				<Link href="/" aria-label="go home" className="mx-auto block size-fit">
+					{ }
+					{brandLogo && (
+						<Image
+							src={(brandLogo as Media).url!}
+							alt={(brandLogo as Media).alt}
+							width={200}
+							height={150}
+						/>
+					)}
+				</Link>
 
-        <div className="flex flex-wrap justify-center gap-6 text-sm">
-          {(links ?? []).map((link, index) => (
-            <Link
-              key={index}
-              href={link.href}
-              className="text-muted-foreground hover:text-primary block duration-150"
-            >
-              <span>{link.label}</span>
-            </Link>
-          ))}
-        </div>
-        <div className="my-8 flex flex-wrap justify-center gap-6 text-sm">
-          {socialLinks.map((icon, index) => (
-            <SocialIcon icon={icon.icon} href={icon.href} key={`${index}`} />
-          ))}
-        </div>
-        <span className="text-muted-foreground block text-center text-sm">
-          {" "}
-          © {new Date().getFullYear()} {copyright}
-        </span>
-      </div>
-    </footer>
-  );
+				<div className="flex flex-wrap justify-center gap-6 text-sm">
+					{(links ?? []).map((link, index) => (
+						<Link
+							key={index}
+							href={link.href}
+							className="text-muted-foreground hover:text-primary block duration-150"
+						>
+							<span>{link.label}</span>
+						</Link>
+					))}
+				</div>
+				<div className="my-8 flex flex-wrap justify-center gap-6 text-sm">
+					{socialLinks.map((icon, index) => (
+						<SocialIcon icon={icon.icon} href={icon.href} key={`${index}`} />
+					))}
+				</div>
+				<span className="text-muted-foreground block text-center text-sm">
+					{" "}
+					© {new Date().getFullYear()} {copyright}
+				</span>
+			</div>
+		</footer>
+	);
 }
