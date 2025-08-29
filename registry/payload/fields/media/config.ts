5db8fd20a86ea8be212175d632f2420d34d6c262
Isaiah Anyimi <41877924+ianyimi@users.noreply.github.com>
2025-08-28 19:39:52 -0700

    feat: some touchups in custom registry code for uifoundry

--- registry/payload/fields/media/config.ts
+++ registry/payload/fields/media/config.ts
@@ -1,4 +1,4 @@
-import type { GroupField } from "@/registry/payload/fields";
+import type { GroupField } from "@/registry/payload/fields/index.d.ts";
 import uploadField from "@/registry/payload/fields/upload/config";
 
 export default function mediaField(props?: Partial<GroupField>): GroupField {
