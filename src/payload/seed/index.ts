0e6a17f8fd1add8c38178cff135c87899ebd9127
Isaiah Anyimi <41877924+ianyimi@users.noreply.github.com>
2025-09-28 02:08:14 -0700

    fix: comment out broken & unfinised seed db code

--- src/payload/seed/index.ts
+++ src/payload/seed/index.ts
@@ -1,16 +1,16 @@
 import type { BasePayload } from "payload";
-import type { Page } from "~/payload-types";
+// import type { Page } from "~/payload-types";
 import { COLLECTION_SLUG_SITES } from "~/payload/constants";
-import home from "./data/sites/uifoundry/pages/home";
+// import home from "./data/sites/uifoundry/pages/home";
 
-async function seedSites(payload: BasePayload) {
-	console.log("homepage: ", home);
+async function seedSites(_payload: BasePayload) {
+	// console.log("homepage: ", home);
 	console.log("seeding sites...");
-	const newPage: Page = {
-		title: home.title,
-		slug: home.slug,
-		blocks: [...home.blocks],
-	};
+	// const newPage: Page = {
+	// 	title: home.title,
+	// 	slug: home.slug,
+	// 	blocks: [...home.blocks],
+	// };
 }
 
 export async function seedDatabase(payload: BasePayload) {
