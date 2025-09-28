7b4b29967ab8a62ea523fd11e24f41d872940217
Isaiah Anyimi <41877924+ianyimi@users.noreply.github.com>
2025-09-28 15:58:11 -0700

    chore: refactor payload seed folder structure to keep collections and data colocated together instead of having the data in its own folder

---
+++ src/payload/seed/collections/sites/index.ts
@@ -0,0 +1,13 @@
+import type { BasePayload } from "payload";
+// import type { Page } from "~/payload-types";
+// import home from "./data/uifoundry/pages/home";
+
+export async function seedSites(_payload: BasePayload) {
+	// console.log("homepage: ", home);
+	console.log("seeding sites...");
+	// const newPage: Page = {
+	// 	title: home.title,
+	// 	slug: home.slug,
+	// 	blocks: [...home.blocks],
+	// };
+}
--- src/payload/seed/index.ts
+++ src/payload/seed/index.ts
@@ -1,17 +1,6 @@
 import type { BasePayload } from "payload";
-// import type { Page } from "~/payload-types";
 import { COLLECTION_SLUG_SITES } from "~/payload/constants";
-// import home from "./data/sites/uifoundry/pages/home";
-
-async function seedSites(_payload: BasePayload) {
-	// console.log("homepage: ", home);
-	console.log("seeding sites...");
-	// const newPage: Page = {
-	// 	title: home.title,
-	// 	slug: home.slug,
-	// 	blocks: [...home.blocks],
-	// };
-}
+import { seedSites } from "./collections";
 
 export async function seedDatabase(payload: BasePayload) {
 	console.log("seeding database...");
