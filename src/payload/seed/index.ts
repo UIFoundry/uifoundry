f6410322fb9b0d9624fe192565f88c7c548f8d98
Isaiah Anyimi <41877924+ianyimi@users.noreply.github.com>
2025-09-28 17:28:02 -0700

    chore: remove console log in main seed script for payload cms, test new gh action deployment scripts

--- src/payload/seed/index.ts
+++ src/payload/seed/index.ts
@@ -3,7 +3,6 @@ import { COLLECTION_SLUG_SITES } from "~/payload/constants";
 import { seedSites } from "./collections";
 
 export async function seedDatabase(payload: BasePayload) {
-	console.log("seeding database...");
 	const sitesRes = await payload.find({
 		collection: COLLECTION_SLUG_SITES,
 	});
